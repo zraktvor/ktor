54370bf1ecc690ea5762a36fddc91968b82002da
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2021-01-22 18:08:48 +0300

    ~ CIO engine startup fix

--- ktor-server/ktor-server-cio/jvm/test/io/ktor/tests/server/cio/CIOEngineTest.kt
+++ ktor-server/ktor-server-cio/jvm/test/io/ktor/tests/server/cio/CIOEngineTest.kt
@@ -5,7 +5,11 @@
 package io.ktor.tests.server.cio
 
 import io.ktor.server.cio.*
+import io.ktor.routing.*
+import io.ktor.application.*
+import io.ktor.response.*
 import io.ktor.server.testing.suites.*
+import kotlinx.coroutines.*
 
 class CIOCompressionTest : CompressionTestSuite<CIOApplicationEngine, CIOApplicationEngine.Configuration>(CIO) {
     init {
@@ -32,5 +36,21 @@ class CIOSustainabilityTest : SustainabilityTestSuite<CIOApplicationEngine, CIOA
     init {
         enableHttp2 = false
         enableSsl = false
+    }
+
+    @kotlin.test.Test
+    fun test() {
+        createAndStartServer {
+            get("/") {
+                call.respondText("Hello, World!\n")
+            }
+        }
+
+        println("Port is $port")
+
+        runBlocking<Unit> {
+            suspendCancellableCoroutine {
+            }
+        }
     }
 }
